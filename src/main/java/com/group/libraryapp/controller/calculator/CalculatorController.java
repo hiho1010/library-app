c6bcc5d1b10d2503b60179d91a8a3e4d05aa6e94
hiho1010 <heeho1998@gmail.com>
2024-02-22 21:18:19 +0900

    make api with DTO

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,13 +1,13 @@
 package com.group.libraryapp.controller.calculator;
 
+import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
     @GetMapping("/add")
-    public int addTwoNumbers(@RequestParam int number1, @RequestParam int number2) {
-        return number1 + number2;
+    public int addTwoNumbers(CalculatorAddRequest request) {
+        return request.getNumber1()+ request.getNumber2();
     }
 }
